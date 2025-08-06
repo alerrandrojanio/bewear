66039724ca318e73885eda0e446cac0013e61f78
Alerrandro Janio <alerrandro.janio07@gmail.com>
2025-08-05 23:34:48 -0300

    feat: sign in and sign up form components

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { SignIn } from "./components/sign-in-form";
+import { SignUp } from "./components/sign-up-form";
+
+export default function Authentication() {
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
+      <Tabs defaultValue="sign-in">
+        <TabsList>
+          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+          <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sign-in">
+          <SignIn />
+        </TabsContent>
+        <TabsContent value="sign-up">
+          <SignUp />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}

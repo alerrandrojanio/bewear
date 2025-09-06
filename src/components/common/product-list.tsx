2a273b87abd6096aed0c8b1aa2bd1927950a64a9
Alerrandro Janio <alerrandro.janio07@gmail.com>
2025-09-05 22:40:45 -0300

    feat: add category selector

--- src/components/common/product-list.tsx
+++ src/components/common/product-list.tsx
@@ -15,7 +15,7 @@ export default function ProductList({ title, products }: ProductListProps) {
   return (
     <div className="space-y-6">
       <h3 className="px-5 font-semibold">{title}</h3>
-      <div className="overflow-x-autopx-5 flex w-full gap-4 [&::-webkit-scrollbar]:hidden">
+      <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
         {products.map((product) => (
           <ProductItem key={product.id} product={product} />
         ))}
